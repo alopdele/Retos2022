b8e0a3b23c8c55ef657ae1ec56f4ae78c59e9832
Alopdele <alopdele@gmail.com>
2023-09-13 18:06:43 +0200

    Reto 29

---
+++ reto29.py
@@ -0,0 +1,17 @@
+"""
+ * Reto #29
+ * ORDENA LA LISTA
+ * Fecha publicación enunciado: 18/07/22
+ * Fecha publicación resolución: 26/07/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea una función que ordene y retorne una matriz de números.
+ * - La función recibirá un listado (por ejemplo [2, 4, 6, 8, 9]) y un parámetro adicional
+ *   "Asc" o "Desc" para indicar si debe ordenarse de menor a mayor o de mayor a menor.
+ * - No se pueden utilizar funciones propias del lenguaje que lo resuelvan automáticamente.
+"""
+def ordenar(lista: list, invertir: bool):
+    return sorted(lista, reverse=invertir)
+
+print(ordenar([1,3,2,5,4], True))
+print(ordenar([1,3,2,5,4], False))
